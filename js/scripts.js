af507bc887bfe7aeb42c25df2c7c954626463cd1
HenriqueFerreira <henrique.ferreira@ads.uniceplac.edu.br>
2024-08-31 20:02:56 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -22,22 +22,22 @@ let monitorWindow;
 
 function atualizarMonitor() {
   if (monitorWindow && !monitorWindow.closed) {
-    monitorWindow.document
-      .getElementById("proximoChamadaMonitor")
-      .querySelector("#nomeAtualTextoMonitor").innerText =
-      proximoChamada.querySelector("#nomeAtualTexto").innerText;
-    monitorWindow.document
-      .getElementById("ultimaChamadaMonitor")
-      .querySelector("#nomeAtualTextoMonitor").innerText =
-      ultimaChamada.querySelector("#nomeAtualTexto").innerText;
-    monitorWindow.document
-      .getElementById("penultimaChamadaMonitor")
-      .querySelector("#nomeAtualTextoMonitor").innerText =
-      penultimaChamada.querySelector("#nomeAtualTexto").innerText;
-    monitorWindow.document
-      .getElementById("antepenultimaChamadaMonitor")
-      .querySelector("#nomeAtualTextoMonitor").innerText =
-      antepenultimaChamada.querySelector("#nomeAtualTexto").innerText;
+    monitorWindow.document.getElementById(
+      "nomeAtualTextoMonitorProximo"
+    ).innerText =
+      proximoChamada.querySelector("#nomeAtualTexto").innerText || "";
+    monitorWindow.document.getElementById(
+      "nomeAtualTextoMonitorUltima"
+    ).innerText =
+      ultimaChamada.querySelector("#nomeAtualTexto").innerText || "";
+    monitorWindow.document.getElementById(
+      "nomeAtualTextoMonitorPenultima"
+    ).innerText =
+      penultimaChamada.querySelector("#nomeAtualTexto").innerText || "";
+    monitorWindow.document.getElementById(
+      "nomeAtualTextoMonitorAntepenultima"
+    ).innerText =
+      antepenultimaChamada.querySelector("#nomeAtualTexto").innerText || "";
   }
 }
 
@@ -53,8 +53,12 @@ function chamarNome(nome) {
   audio.play();
 
   // Adiciona o nome na fila de chamadas, se não estiver lá
-  if (!filaNomes.includes(nome)) {
-    filaNomes.unshift(nome);
+  if (!filaNomes.some((item) => item.nome === nome)) {
+    filaNomes.unshift({
+      nome: nome,
+      data: new Date().toLocaleDateString(),
+      hora: new Date().toLocaleTimeString(),
+    });
     atualizarNomesExibidos();
   }
 
@@ -64,27 +68,30 @@ function chamarNome(nome) {
 
 // Atualiza os elementos da tela com os nomes
 function atualizarNomesExibidos() {
-  if (filaNomes.length > 1) {
-    proximoChamada.querySelector("#nomeAtualTexto").innerText = filaNomes[0];
+  if (filaNomes.length > 0) {
+    proximoChamada.querySelector("#nomeAtualTexto").innerText =
+      filaNomes[0].nome;
   } else {
     proximoChamada.querySelector("#nomeAtualTexto").innerText = "";
   }
 
-  if (filaNomes.length > 2) {
-    ultimaChamada.querySelector("#nomeAtualTexto").innerText = filaNomes[1];
+  if (filaNomes.length > 1) {
+    ultimaChamada.querySelector("#nomeAtualTexto").innerText =
+      filaNomes[1].nome;
   } else {
     ultimaChamada.querySelector("#nomeAtualTexto").innerText = "";
   }
 
-  if (filaNomes.length > 3) {
-    penultimaChamada.querySelector("#nomeAtualTexto").innerText = filaNomes[2];
+  if (filaNomes.length > 2) {
+    penultimaChamada.querySelector("#nomeAtualTexto").innerText =
+      filaNomes[2].nome;
   } else {
     penultimaChamada.querySelector("#nomeAtualTexto").innerText = "";
   }
 
-  if (filaNomes.length > 4) {
+  if (filaNomes.length > 3) {
     antepenultimaChamada.querySelector("#nomeAtualTexto").innerText =
-      filaNomes[3];
+      filaNomes[3].nome;
   } else {
     antepenultimaChamada.querySelector("#nomeAtualTexto").innerText = "";
   }
@@ -130,43 +137,46 @@ document.addEventListener("keydown", function (event) {
 document.getElementById("abrirMonitor").addEventListener("click", function () {
   monitorWindow = window.open("", "monitor", "width=1920,height=1080");
   monitorWindow.document.write(`
-    <html>
-    <head>
-      <title>Monitor de Senhas</title>
-      <link href="./css/style.css" rel="stylesheet" />
-    </head>
-    <body>
-    <div class="container page">
-      <!-- Barra Superior com a Logo e o Texto -->
-      <div class="row barraSuperior">
-        <div class="col-xs-2">
-          <img src="/images/SEPD.png" class="imageLogo" />
-        </div>
-        <div class="col-xs-10">
-          <span class="centralTexto">CONTROLE DE SENHA - GAMA</span>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row main-content">
-          <div class="col-md-6 boxCentralizado">
-            <div class="pessoaBox" id="proximoChamadaMonitor">
-              <span id="nomeAtualTextoMonitor"></span>
+    <!DOCTYPE html>
+    <html lang="PT-BR">
+      <head>
+        <meta charset="UTF-8" />
+        <title>Monitor de Senhas</title>
+        <link href="./css/style.css" rel="stylesheet" />
+      </head>
+      <body>
+        <div class="container page">
+          <!-- Barra Superior com a Logo e o Texto -->
+          <div class="row barraSuperior">
+            <div class="col-xs-2">
+              <img src="/images/SEPD.png" class="imageLogo" />
+            </div>
+            <div class="col-xs-10">
+              <span class="centralTexto">CONTROLE DE SENHA - GAMA</span>
             </div>
           </div>
-          <div class="col-md-6 boxDireita">
-            <div class="pessoaBox" id="ultimaChamadaMonitor">
-              <span id="nomeAtualTextoMonitor"></span>
-            </div>
-            <div class="pessoaBox" id="penultimaChamadaMonitor">
-              <span id="nomeAtualTextoMonitor"></span>
-            </div>
-            <div class="pessoaBox" id="antepenultimaChamadaMonitor">
-              <span id="nomeAtualTextoMonitor"></span>
+          <div class="container">
+            <div class="row main-content">
+              <div class="col-md-6 boxCentralizado">
+                <div class="pessoaBox" id="proximoChamadaMonitor">
+                  <span id="nomeAtualTextoMonitorProximo"></span>
+                </div>
+              </div>
+              <div class="col-md-6 boxDireita">
+                <div class="pessoaBox" id="ultimaChamadaMonitor">
+                  <span id="nomeAtualTextoMonitorUltima"></span>
+                </div>
+                <div class="pessoaBox" id="penultimaChamadaMonitor">
+                  <span id="nomeAtualTextoMonitorPenultima"></span>
+                </div>
+                <div class="pessoaBox" id="antepenultimaChamadaMonitor">
+                  <span id="nomeAtualTextoMonitorAntepenultima"></span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </body>
+      </body>
     </html>
   `);
 
@@ -177,35 +187,70 @@ document.getElementById("abrirMonitor").addEventListener("click", function () {
 // Função para zerar todas as chamadas
 document.getElementById("zerarChamadas").addEventListener("click", function () {
   filaNomes.length = 0; // Limpa a fila de nomes
-  proximoChamada.querySelector("#nomeAtualTexto").innerText = "";
-  ultimaChamada.querySelector("#nomeAtualTexto").innerText = "";
-  penultimaChamada.querySelector("#nomeAtualTexto").innerText = "";
-  antepenultimaChamada.querySelector("#nomeAtualTexto").innerText = "";
+  atualizarNomesExibidos(); // Atualiza a tela principal
 
   // Atualiza o monitor se estiver aberto
   atualizarMonitor();
 });
 
 // Função para exportar para CSV
-// Função para exportar os dados para Excel
-
 document.getElementById("exportarCSV").addEventListener("click", function () {
   if (filaNomes.length === 0) {
     alert("Não há senhas para exportar.");
     return;
   }
 
-  let csvContent = "data:text/csv;charset=utf-8,Nome,Horário\n";
-  filaNomes.forEach(function (nome) {
-    let horario = new Date().toLocaleTimeString();
-    csvContent += `"${nome}","${horario}"\n`;
+  // Dados para o CSV
+  let csvContent = "data:text/xls;charset=utf-8,Data,Hora,Nome Chamado\n";
+
+  filaNomes.forEach(function (item) {
+    csvContent += `"${item.data}","${item.hora}","${item.nome}"\n`;
   });
+
+  // Adiciona um resumo no final
+  let totalChamadas = filaNomes.length;
+  csvContent += `\nResumo\nTotal de Chamadas,${totalChamadas}`;
 
   const encodedUri = encodeURI(csvContent);
   const link = document.createElement("a");
   link.setAttribute("href", encodedUri);
-  link.setAttribute("download", "relatorio_servicos.csv");
+  link.setAttribute("download", "atendimentos.xls");
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+});
+
+// Função para exportar para XLS
+document.getElementById("exportarXLS").addEventListener("click", function () {
+  if (filaNomes.length === 0) {
+    alert("Não há senhas para exportar.");
+    return;
+  }
+
+  // Dados para o XLS
+  const data = [];
+  const headers = ["Data", "Hora", "Nome Chamado"];
+  data.push(headers);
+
+  filaNomes.forEach(function (item) {
+    const row = [item.data, item.hora, item.nome];
+    data.push(row);
+  });
+
+  // Adiciona o resumo
+  const resumoData = [
+    ["Resumo"],
+    ["Total de Serviços Atendidos", filaNomes.length],
+  ];
+
+  // Cria a planilha e o arquivo
+  const wb = XLSX.utils.book_new();
+  const ws = XLSX.utils.aoa_to_sheet(data);
+  XLSX.utils.book_append_sheet(wb, ws, "Chamadas");
+
+  const wsResumo = XLSX.utils.aoa_to_sheet(resumoData);
+  XLSX.utils.book_append_sheet(wb, wsResumo, "Resumo");
+
+  // Salva o arquivo
+  XLSX.writeFile(wb, "atendimentos.xlsx");
 });
